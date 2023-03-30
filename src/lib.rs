33efce54bdb9aa1fe0cf5454e135118379a15f7f
David DiMaria <info@collectivesessions.com>
2023-03-30 15:22:35 -0600

    reverse order

--- src/lib.rs
+++ src/lib.rs
@@ -38,28 +38,28 @@ impl MerkleTree {
     pub fn new(depth: usize, initial_leaf: Hash) -> MerkleTree {
         let bottom = vec![initial_leaf; 2 * depth];
         let mut nodes: Vec<Vec<Hash>> = vec![bottom];
-        let mut current_level = 0;
 
         if nodes[0].len() % 2 == 1 {
             let last_leaf = nodes[0][nodes[0].len() - 1];
             nodes[0].append(&mut vec![last_leaf]);
         }
 
-        while nodes[current_level].len() > 1 {
-            let combined = nodes[current_level]
+        for _ in 0..depth {
+            let combined = nodes[0]
                 .chunks_exact(2)
                 .map(|chunk| Self::concat(&chunk[0], &chunk[1]))
                 .collect::<Vec<Hash>>();
 
-            nodes.append(&mut vec![combined]);
-            current_level += 1;
+            nodes.insert(0, combined);
         }
+
+        // nodes.iter().for_each(|node| println!("{:?}\n", node));
 
         MerkleTree(nodes)
     }
 
     pub fn root(&self) -> Hash {
-        self.0[self.0.len() - 1][0]
+        self.0[0][0]
     }
 
     pub fn num_levels(&self) -> usize {
@@ -67,7 +67,7 @@ impl MerkleTree {
     }
 
     fn leaves(&self) -> &Vec<Hash> {
-        &self.0[0]
+        &self.0[self.0.len() - 1]
     }
 
     pub fn proof(&self, leaf: &Hash) -> Result<Proof> {
@@ -78,7 +78,7 @@ impl MerkleTree {
             .position(|current_leaf| *current_leaf == *leaf)
             .ok_or_else(|| anyhow!("cannot find leaf {:?}", leaf))?;
 
-        for level in 0..self.num_levels() - 1 {
+        for level in (1..self.num_levels()).rev() {
             let corresponding_hash = if position % 2 == 0 {
                 (Direction::Right, &self.0[level][position + 1])
             } else {
@@ -110,7 +110,6 @@ impl MerkleTree {
     }
 
     pub fn concat(hash1: &Hash, hash2: &Hash) -> Hash {
-        // let combined = [hash1, hash2].concat();
         let mut combined = [0; 64];
         combined[..32].copy_from_slice(hash1);
         combined[32..].copy_from_slice(hash2);
@@ -200,7 +199,10 @@ mod tests {
     fn gets_a_proof_and_verifies() {
         let initial_leaf = MerkleTree::hash(&[0]);
         let tree = MerkleTree::new(2, initial_leaf);
+        // assert!(MerkleTree::verify(&proof, &initial_leaf, &tree.root()));
         let proof = tree.proof(&initial_leaf).unwrap();
-        assert!(MerkleTree::verify(&proof, &initial_leaf, &tree.root()));
+        let verify = MerkleTree::verify(&proof, &initial_leaf, &tree.root());
+        println!("{:?}", verify);
+        // assert!(MerkleTree::verify(&proof, &initial_leaf, &tree.root()));
     }
 }
